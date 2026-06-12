56021b06e18534c57705355d0a77c27668e23a6d
Sravanitest123 <sravani.a2704@gmail.com>
2026-06-12 16:06:42 +0530

    thirdcommit

--- src/test/java/Demo/SampleTest.java
+++ src/test/java/Demo/SampleTest.java
@@ -8,6 +8,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.amazon.com");
+	driver.manage().window().maximize();
 
 	}
 
